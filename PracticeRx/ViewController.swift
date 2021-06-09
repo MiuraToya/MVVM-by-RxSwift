0177688c7afb7c5c03c980c71dbb5332304e185e
Miura Toya <dengzais8@gmail.com>
2021-06-09 23:30:30 +0900

    Initial Commit

---
+++ PracticeRx/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeRx
+//
+//  Created by 三浦　登哉 on 2021/06/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
